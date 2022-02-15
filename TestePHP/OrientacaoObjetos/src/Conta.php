001602cd5afe51ff0ec9e7b806dc4dd4cc715617
Adriano Carneiro <adrianodoss@gmail.com>
2022-02-15 10:55:43 -0300

    15/02

--- TestePHP/OrientacaoObjetos/src/Conta.php
+++ TestePHP/OrientacaoObjetos/src/Conta.php
@@ -5,4 +5,29 @@ class Conta
     public $cpf; // a partir do 7.4     public String $cpf;
     public $nome;
     public $saldo;
+}    public $saldo = 0;
+    {
+        if ($valorASacar > $this->saldo) {
+            echo "Saldo indisponivel";
+            return;
+        }
+        $this->saldo -= $valorASacar;
+    }
+    public function depositar(float $valorADepositar)
+    {
+        if ($valorADepositar < 0) {
+            echo "valor presisa ser positivo";
+            return;
+        }
+        $this->saldo += $valorADepositar;
+    }
+    public function transferir($valoraTransferir, $contaDestino)
+    {
+        if ($valoraTransferir > $this->saldo) {
+            echo "valor indisponivel";
+            return;
+        }
+        $this->sacar($valoraTransferir);
+        $contaDestino->depositar($valoraTransferir);
+    }
 }
